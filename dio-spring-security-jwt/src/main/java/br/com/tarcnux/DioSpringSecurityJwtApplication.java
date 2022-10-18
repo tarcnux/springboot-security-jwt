699da26e695f212adb290ed518037b02011fcba8
Tarcisio Nunes <tarcnux@gmail.com>
2022-10-18 16:58:33 -0300

    Projeto inicial
    Ao inicializar o projeto é gerado um password, o usuário é user

---
+++ dio-spring-security-jwt/src/main/java/br/com/tarcnux/DioSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package br.com.tarcnux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringSecurityJwtApplication.class, args);
+	}
+
+}
